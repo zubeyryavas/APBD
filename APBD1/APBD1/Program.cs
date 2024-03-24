934fd05cb7d70d210d6a3970aaceb7ee630fb7fe
zubeyryavas <yavaszubeyr@gmail.com>
2024-03-24 21:28:49 +0100

    Add max method

--- APBD1/APBD1/Program.cs
+++ APBD1/APBD1/Program.cs
@@ -4,10 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        int[] intArray = {1, 2, 6};
-        int average = Average(intArray);
+        int[] intArray = {1, 2, 5, 6, 5, 5};
+     
 
-        Console.WriteLine(average);
+        Console.WriteLine("Average value is " + Average(intArray));
+        Console.WriteLine("Max value is " + FindMax(intArray));
     }
 
     public static int Average(params int[] intArray)
@@ -20,5 +21,20 @@ class Program
         }
 
         return sum / intArray.Length;
+    }
+
+    public static int FindMax(params int[] intArray)
+    {
+        int maxInt = Int32.MinValue;
+
+        for (int i = 0; i < intArray.Length; i++)
+        {
+            int value = intArray[i];
+            if (value > maxInt)
+            {
+                maxInt = value;
+            }
+        }
+        return maxInt;
     }
 }
