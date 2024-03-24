cece70c912ffe3b4050860be5debf6f11afe7602
zubeyryavas <yavaszubeyr@gmail.com>
2024-03-24 19:28:22 +0100

    average function added

--- APBD1/APBD1/Program.cs
+++ APBD1/APBD1/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Modification 2!");
+﻿namespace APBD1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] intArray = {1, 2, 6};
+        int average = Average(intArray);
+
+        Console.WriteLine(average);
+    }
+
+    public static int Average(params int[] intArray)
+    {
+        int sum = 0;
+
+        for (int i = 0; i < intArray.Length; i++)
+        {
+            sum += intArray[i];
+        }
+
+        return sum / intArray.Length;
+    }
+}
