0f965684dac9d1bcf6789702d68ca6a18d2eb5ed
zubeyryavas <yavaszubeyr@gmail.com>
2024-03-24 21:28:49 +0100

    Change from sum to total

--- APBD1/APBD1/Program.cs
+++ APBD1/APBD1/Program.cs
@@ -4,21 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        int[] intArray = {1, 2, 6};
-        int average = Average(intArray);
+        int[] intArray = {1, 2, 5, 6, 5, 5};
+     
 
-        Console.WriteLine(average);
+        Console.WriteLine("Average value is " + Average(intArray));
+        Console.WriteLine("Max value is " + FindMax(intArray));
     }
 
     public static int Average(params int[] intArray)
     {
-        int sum = 0;
+        int total = 0;
 
         for (int i = 0; i < intArray.Length; i++)
         {
-            sum += intArray[i];
+            total += intArray[i];
         }
 
-        return sum / intArray.Length;
+        return total / intArray.Length;
+    }
+
+    public static int FindMax(params int[] intArray)
+    {
+        int maxInt = Int32.MinValue;
+
+        for (int i = 0; i < intArray.Length; i++)
+        {
+            int value = intArray[i];
+            if (value > maxInt)
+            {
+                maxInt = value;
+            }
+        }
+        return maxInt;
     }
 }
